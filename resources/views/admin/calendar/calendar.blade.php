07490b3bc5f773e2bf0a893d22b8a809d8110a55
Herick <herickcwilke@gmail.com>
2019-11-21 12:57:00 -0300

    Calendario atualizado

--- resources/views/admin/calendar/calendar.blade.php
+++ resources/views/admin/calendar/calendar.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.admin')
 @section('content')
 <div class="content">
-    <h3 class="page-title">{{ trans('global.systemCalendar') }}</h3>
+    <h3 class="page-title">Calendário de Chamados</h3>
     <div class="row">
         <div class="col-lg-12">
 
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    {{ trans('global.systemCalendar') }}
+                    O calendário apresenta o prazo final de entrega dos chamados abertos.
                 </div>
                 <div class="panel-body">
 
